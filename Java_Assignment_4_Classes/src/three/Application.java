52eaee526bfb1ca8d29538dd61a0c0fd4784b6dd
Ricky Brown <rickyb4536@gmail.com>
2018-02-23 14:53:40 -0500

    Second Commit
    More class access

---
+++ Java_Assignment_4_Classes/src/three/Application.java
@@ -0,0 +1,11 @@
+package three;
+
+class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TalkablePerson tp = new TalkablePerson("Rick", 25);
+		tp.talk();
+	}
+
+}
---
+++ Java_Assignment_4_Classes/src/three/TalkablePerson.java
@@ -0,0 +1,17 @@
+package three;
+
+public class TalkablePerson {
+
+	String name; 
+	int age;
+	
+	public TalkablePerson(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+	
+	public void talk() {
+		System.out.println("Hello, m name is: " + this.name);
+	} 
+}

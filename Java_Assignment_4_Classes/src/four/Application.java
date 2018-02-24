ed758e6cad4e18d18b53af25c5b2b01fda80f170
Ricky Brown <rickyb4536@gmail.com>
2018-02-23 22:53:31 -0500

    final commit
    more of accessing data from a different class

---
+++ Java_Assignment_4_Classes/src/four/Application.java
@@ -0,0 +1,11 @@
+package four;
+
+class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ConstructorPerson cp = new ConstructorPerson();
+		System.out.println("Name: " + cp.name +  ", Age:  " + cp.age);
+	}
+
+}
---
+++ Java_Assignment_4_Classes/src/four/ConstructorPerson.java
@@ -0,0 +1,11 @@
+package four;
+
+public class ConstructorPerson {
+	String name;
+	int age;
+	
+	public ConstructorPerson() {
+		this.name = "First name";
+		this.age = 19;
+	}
+}
